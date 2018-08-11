cb7af4246b450e960f401024fa90f0578a6f9411
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-11 15:33:09 -0400

    finished lesson

--- ViewController.swift
+++ ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func helloButton(_ sender: Any) {
+        myLabel.text = "Hello World!"
+    }
+    @IBAction func goodbyeButton(_ sender: Any) {
+        myLabel.text = "Goodbye World!"
+    }
+    @IBAction func unicornButton(_ sender: Any) {
+        myLabel.text = "Unicorn Emoji"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myLabel.text = "?"
     }
 
     override func didReceiveMemoryWarning() {
